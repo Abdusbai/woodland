a786704aa07fb31c82bd165161b415cdb7b3f25e
Abdessamad Sbai <sbai.itsd@gmail.com>
2023-09-28 17:29:21 +0100

    adding supabae.js file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://rtybtovqwycqgnatkeat.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0eWJ0b3Zxd3ljcWduYXRrZWF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5MTA5NjksImV4cCI6MjAxMTQ4Njk2OX0.SH3nZOijmSM79KAaZ5VXomE3ONS534woZ6uFsNb-mPE";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
