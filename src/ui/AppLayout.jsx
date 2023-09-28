964664baad803abe823f25f026deed098043e998
Abdessamad Sbai <sbai.itsd@gmail.com>
2023-09-28 15:18:22 +0100

    finishing design and adding icons to the sidebar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,29 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100dvh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
-      <p>App Layout</p>
-      <Outlet />
-    </div>
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import Logo from "../ui/Logo";
+import MainNav from "../ui/MainNav";
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+  grid-row: 1/-1;
+  display: flex;
+  flex-direction: column;
+  gap: 2.6rem;
+`;
+
+function Sidebar() {
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
+}
+
+export default Sidebar;
